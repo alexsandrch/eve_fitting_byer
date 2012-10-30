bfb2f90e1ef4f20b68141b691b4430a09d2a12a6
alexsandrch-ch <alexsandrch-ch@gmail.com>
2012-10-30 19:13:13 +0500

    Первый коммит
    Коммит для теста GIT и заливки файлов проекта

---
+++ src/internalFunction/ProjectLauncher.java
@@ -0,0 +1,16 @@
+package internalFunction;
+
+import projectWindows.*;
+
+public class ProjectLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ByerWindow byer_window = new ByerWindow("EVE SIMPLE FITTING BYING PROGRAM");
+		byer_window.setVisible(true);
+	}
+
+}
---
+++ src/projectWindows/ByerWindow.java
@@ -0,0 +1,13 @@
+package projectWindows;
+
+import javax.swing.JFrame;
+
+public class ByerWindow extends JFrame {
+ 
+  public ByerWindow(String title) {
+	  super(title);
+	  setSize(640, 480);
+	  
+  }
+
+}
